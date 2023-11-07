c03f955a2d3acab6e666935af06e55b823c698f8
Endre Márk Borza <endremborza@gmail.com>
2023-11-07 16:22:00 +0100

    load dotenv

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,9 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 RAW_ROOT = Path(os.environ["RAW_SCIENCE_DATA_PATH"])
 PARSED_ROOT = Path(os.environ["PARSED_SCIENCE_DATA_PATH"])
