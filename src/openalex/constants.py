bd369ac86b11a127479bfb3d6c0a36b11c17b7c7
Endre Márk Borza <endremborza@gmail.com>
2023-11-07 16:21:42 +0100

    add id maps base

--- src/openalex/constants.py
+++ src/openalex/constants.py
@@ -2,7 +2,8 @@ from ..constants import PARSED_ROOT, RAW_ROOT
 
 SNAPSHOT_PATH = RAW_ROOT / "openalex-snapshot"
 
-PARTITIONED_CSV_PATH = PARSED_ROOT / "openalex-partitioned-csv"
+PARTITIONED_CSV_PATH = RAW_ROOT / "openalex-partitioned-csv"
 PARQUET_BLOB_ROOT = PARSED_ROOT / "openalex-parquet"
+ID_MAP_ROOT = PARSED_ROOT / "id-maps"
 
 N_GROUPS = 128
